1a1200dde8d19975a67c39a99511b566c4e84996
Cassio C <LccassioL@gmail.com>
2021-04-24 16:43:58 -0300

    create first repositorie

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,4 @@ import "./database";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Ola mundo"});
-});
-
 app.listen(3333, () => console.log("Opening the gates"))
