8400c4d22d37c244c20a1b90f97ccbf9d35bba7b
Cassio C <LccassioL@gmail.com>
2021-04-25 17:05:38 -0300

    implements socket connection and page with view engine

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,4 @@
-import express from 'express';
-import "./database";
-import { routes } from './routes';
+import { http } from './http';
+import "./websocket/client";
 
-const app = express();
-app.use(express.json());
-app.use(routes);
-
-app.listen(3333, () => console.log("Opening the gates"))
+http.listen(3333, () => console.log("Opening the gates"))
---
+++ src/websocket/client.ts
@@ -0,0 +1,51 @@
+import { io } from '../http';
+import { ConnectionsService } from '../services/ConnectionsService';
+import { UsersService } from '../services/UsersService';
+import { MessagesService } from '../services/MessagesService';
+
+interface IParams {
+  text: string;
+  email: string;
+}
+
+io.on("connect", (socket) => {
+  const connectionService = new ConnectionsService();
+  const usersService = new UsersService();
+  const messagesService = new MessagesService();
+
+  socket.on("client_first_access", async (params) => {
+    const socket_id = socket.id;
+    const { text, email } = params as IParams;
+    let user_id = null;
+
+    const userExists = await usersService.findByEmail(email);
+    user_id = userExists.id;
+
+    if(userExists){
+      const connection = await connectionService.findByUserId(userExists.id);
+
+      if(connection){
+        await connectionService.create({
+          socket_id,
+          user_id: userExists.id
+        });
+      }else{
+        connection.socket_id = socket_id;
+        await connectionService.create(connection);
+      }
+    }else{
+      const user = await usersService.create(email);
+      user_id = user.id;
+      
+      await connectionService.create({
+        socket_id,
+        user_id: user.id
+      });
+    }
+
+    await messagesService.create({
+      text,
+      user_id
+    })
+  });
+});
